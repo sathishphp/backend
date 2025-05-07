37f081a2b2110a65c66da9b9a0b664d670e63716
sathishphp <35943038+sathishphp@users.noreply.github.com>
2025-05-07 19:48:32 +0530

    Routing URL Update

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 const companyRouter = require("./routes/company.routes");
 const branchRouter = require("./routes/branch.routes");
+const shedRouter = require("./routes/shed.routes");
+const machineBrandRouter = require("./routes/machine-brand.routes");
+const departmentRouter = require("./routes/department.routes");
+const designationRouter = require("./routes/designation.routes");
 const employeeRouter = require("./routes/employee.routes");
 const app = express();
 app.use(cors());
@@ -17,6 +21,10 @@ app.use(bodyParser.json());
 app.use("/api/v1/employee", employeeRouter);
 app.use("/api/v1/company",auth.authenticateToken, companyRouter);
 app.use("/api/v1/branch",auth.authenticateToken, branchRouter);
+app.use("/api/v1/shed",auth.authenticateToken, shedRouter);
+app.use("/api/v1/department",auth.authenticateToken, departmentRouter);
+app.use("/api/v1/designation",auth.authenticateToken, designationRouter);
+app.use("/api/v1/machine-brand",auth.authenticateToken, machineBrandRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
